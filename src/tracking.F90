ac336272755145d4b10f809c6da2b5f77fdfba19
Jose Salcedo Perez <salcedop@jlselogin2.ftm.alcf.anl.gov>
2018-03-26 19:31:50 +0000

    cleaning out, nothing major.

--- src/tracking.F90
+++ src/tracking.F90
@@ -328,11 +328,14 @@ contains
           ! Enter new particle in particle track file
           if (p % write_track) call add_particle_track()
         else
-
+         if idx \= 0 then
          call flush_buffer(buffer_index(:,:,1:idx),&
          buffer_interp(:,1:idx),buffer_materials(1:idx),&
          buffer_distances(1:idx),buffer_flag(1:idx),idx)
          idx = 0
+         else
+           idx=idx
+         end if
           exit EVENT_LOOP
         end if
       end if
